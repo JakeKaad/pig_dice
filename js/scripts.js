0f85c71a07c98ccb63d0638c63ee6e505114d59f
Jake Kaad <kaadalac@gmail.com>
2015-02-19 07:08:42 -0800

    Add player one turn

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,28 @@
 
 var Dice = {
   roll: function() {
-    return (Math.floor(Math.random() * (6) + 1)).toString();
+    return (Math.floor(Math.random() * (6) + 1));
   }
 };
 
 var Turn = {
   score: 0,
+  stillPlayerTurn: true,
   updateScore: function(side) {
     if(side === 1) {
       this.score = 0
-      return 'turn over'
+      this.stillPlayerTurn = false;
     } else {
-      this.score += side ;
-      return 'roll again'
+      this.score = this.score + side;
     }
   }
 };
 
 var Player = {
   totalScore: 0,
-  turn: false,
   di: Object.create(Dice),
   updateScore: function(turnScore){
-    this.totalScore += parseInt(turnScore);
+    this.totalScore += turnScore;
   },
   checkForWinner: function(turnScore) {
     return (this.totalScore + turnScore) >= 100
@@ -37,21 +36,8 @@ var Player = {
     return this.di.roll();
   },
   takeTurn: function() {
-    var newTurn = Object.create(Turn);
-    console.log(this.username + "'s turn'")
-    for(var i = 0; i < 51; i++ ) {
-      var result = newTurn.updateScore(this.rollDice());
-      console.log("Your Score: " + this.totalScore);
-      console.log("Turn Score: " + newTurn.score);
-      if (this.checkForWinner(newTurn.score)){
-        this.turn = true;
-      }
-      this.passTurn(result)
-      yesOrNo = confirm("Would you like to roll again?")
-      if (this.turn === false){
-        break;
-      }
-    }
+    this.newTurn = Object.create(Turn);
+    return this.newTurn
   }
 };
 
@@ -73,13 +59,36 @@ $(function(){
   $('#new_game').click(function(){
     newGame = Object.create(Game);
     newGame.setUp();
-    $('.player_one_name').text(newGame.playerOne.username)
-    $('#player_one_score').text(newGame.playerOne.totalScore)
-    $('.player_two_name').text(newGame.playerTwo.username)
-    $('#player_two_score').text(newGame.playerTwo.totalScore)
+    var playerOne = newGame.playerOne
+    var playerTwo = newGame.playerTwo
+    $('.player_one_name').text(playerOne.username)
+    $('#player_one_score').text(playerOne.totalScore)
+    $('.player_two_name').text(playerTwo.username)
+    $('#player_two_score').text(playerTwo.totalScore)
     $('.player-squares').show();
 
-   });
+    var playerOneTurn = playerOne.takeTurn();
+    $('#player_one_button').click(function(){
+      var pOneRoll = playerOne.rollDice();
+      $('#player_one_last_roll').text(pOneRoll);
+      playerOneTurn.updateScore(pOneRoll);
+      if (playerOneTurn.stillPlayerTurn) {
+        $('#player_one_turn_score').text(playerOneTurn.score);
+      } else {
+        $('#player_one_turn_area').hide();
+        $('#player_two_turn_area').show();
+      }
+
+      $('#player_one_pass_button').show();
+      $('#player_one_pass_button').click(function(){
+        playerOne.updateScore(playerOneTurn.score);
+        $('#player_one_score').text(playerOne.totalScore);
+        playerTwo.takeTurn();
+        $('#player_one_turn_area').hide();
+        $('#player_two_turn_area').show();
+      });
+    });
+  });
 });
 
 
